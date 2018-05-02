1599921ff9befb30ac6f9eafb2b12c10e165b6eb
pushpinder <pushp@DESKTOP-3EEE01S>
2018-05-02 12:01:04 -0400

    Add MVC web project

---
+++ Training Code/Week 3/Tapas/Tapas.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tapas.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        string name = "Nick Escalona";
+        int num = 10;
+        
+        //Action Methods
+        // GET: Home
+        public string Hello()
+        {
+            return "Hello world";
+        }
+        public string Welcome()
+        {
+            return "Welcome to <b> USF-Revature</b>";
+        }
+        //[NonAction]
+        public ContentResult Test()
+        {
+            return Content(HttpUtility.HtmlEncode("This is the .Net <script>alert('Your System is Hacked')</script> Training program"));
+        }
+        public ActionResult Index()
+        {
+            TempData["Training"] = ".Net Full Stack";
+            return View();
+        }
+        public ActionResult TestIndex()
+        {
+            TempData.Keep("Training");
+            return View();
+        }
+       
+    }
+}
---
+++ Training Code/Week 3/Tapas/Tapas.Web/Models/Restaurant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Tapas.Web.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Zipcode { get; set; }
+        public string Address
+        {
+            get
+            {
+                return Street1 + " ," + Street2 + " ," + City + " ," + State + " ," + Country + " ," + Zipcode;
+            }
+
+        }
+        static List<Restaurant> restaurants = new List<Restaurant>() {
+                new Restaurant(){Id=1,Street1="Elden st", Street2="123", Name="Paradise", City="Reston", State="VA",Country="US",Zipcode="10130"},
+                new Restaurant(){Id=2,Street1="Elden st", Street2="123", Name="Wendy's", City="Reston", State="VA",Country="US",Zipcode="20130 "},
+                new Restaurant(){Id=3,Street1="Elden st", Street2="123", Name="Mod's", City="Reston", State="VA",Country="US",Zipcode="20130 " }
+            };
+        public IEnumerable<Restaurant> GetRestaurants()
+        {           
+            return restaurants;
+        }
+        public Restaurant GetRestaurantById(int id)
+        {
+            return restaurants.Where(x=>x.Id==id).FirstOrDefault();
+        }
+        public void AddRestaurant(Restaurant restaurant)
+        {
+            restaurants.Add(restaurant);
+            Debug.Write("Added 1 Restaurant");
+        }
+
+    }
+}
