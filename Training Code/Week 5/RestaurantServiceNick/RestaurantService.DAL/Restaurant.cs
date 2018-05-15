e463826a74070322a4cc34bc93aa38f229ad9f4d
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-05-15 15:20:14 -0400

    add nick's wcf data service example

---
+++ Training Code/Week 5/RestaurantServiceNick/RestaurantService.DAL/Restaurant.cs
@@ -0,0 +1,34 @@
+namespace RestaurantService.DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Restaurant
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Restaurant()
+        {
+            Reviews = new HashSet<Review>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ Training Code/Week 5/RestaurantServiceNick/RestaurantService.SL/IRestaurantService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace RestaurantService.SL
+{
+    [ServiceContract]
+    public interface IRestaurantService
+    {
+        [OperationContract]
+        IEnumerable<Restaurant> GetAll();
+
+        [OperationContract]
+        Restaurant Get(int id);
+    }
+}
