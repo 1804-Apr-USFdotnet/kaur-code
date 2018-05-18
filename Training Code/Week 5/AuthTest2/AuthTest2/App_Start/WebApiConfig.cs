9ab2900973f6bf9ffc9fed4976cdcda9b6e7764a
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-05-18 11:10:25 -0400

    add web api with owin cookie auth

---
+++ Training Code/Week 5/AuthTest2/AuthTest2/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace AuthTest2
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // equivalent of [Authorize] on EVERYTHING
+            config.Filters.Add(new AuthorizeAttribute());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Training Code/Week 5/AuthTest2/AuthTest2/Controllers/AdminDataController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AuthTest2.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class AdminDataController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok("You're an Admin!");
+        }
+    }
+}
